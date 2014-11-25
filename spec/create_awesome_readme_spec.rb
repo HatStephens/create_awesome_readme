187e59302c7c25e0ac4607e894e2e0e132cee4fd
Stephen Giles <s.giles@hotmail.co.uk>
2014-11-25 23:47:40 +0000

    Removed old folder

--- spec/create_awesome_readme_spec.rb
+++ spec/create_awesome_readme_spec.rb
@@ -4,9 +4,9 @@ describe CreateAwesomeReadme do
 
   let(:readme){CreateAwesomeReadme.new}
 
-  # it 'should create a blank file' do
-  #   readme.create_new
-  #   expect(File).to exist("./in_here/an_awesome_readme.md")
-  # end
+  it 'should create a blank file' do
+    readme.create_new
+    expect(File).to exist("./in_here/an_awesome_readme.md")
+  end
 
 end
